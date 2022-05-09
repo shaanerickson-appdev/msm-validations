08fd804e8e35e6b86a61903b37bcb858455024ae
Shaan <shaan.erickson@chicagobooth.edu>
2022-05-09 13:40:06 -0500

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,8 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  validates(:director_id, { :presence => true })
+  validates(:title, { :uniqueness => { :scope => [:year]} })
   def director
     my_director_id = self.director_id
 
@@ -21,5 +23,13 @@ class Movie < ApplicationRecord
     the_director = matching_directors.at(0)
 
     return the_director
+  end
+
+  def director_name
+    if self.director
+      return self.director.name
+    else
+      return "No director found"
+    end
   end
 end
